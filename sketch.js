98db558125d745b543de2a579ad5e5da71a5fb37
Amber <amberj.kolar@gmail.com>
2017-10-10 21:03:57 -0700

    Added a human circle to fight the zombie, made circle properties random

--- sketch.js
+++ sketch.js
@@ -1,55 +1,102 @@
 //Zombulator by Amber Kolar
 
-var zombieX = 200;
+/*var zombieX = 200;
 var zombie2X = 50;
-var zombieY = 100;
-var zombieV = 1;
-var zombieA = 0.2;
-var zombieDamping = -0.8;
-var zombieSize = 80;
-//var redColor = 255;
-var zombieColor;
-var backgroundColor;
-
-var humanY = 100;
-var humanSize = 80;
-
 var speed = 3;
 var size1 = 80;
-var size2 = 80;
+var size2 = 80;*/
+
+var blood1 = 255;
+var blood2 = 255;
+
+var zombieY = 100;
+var zombieV = 0;
+var zombieA;
+var zombieDamping;
+var zombieLoss;
+var zombieSize;
+var zombieColor;
+
+var humanY = 650;
+var humanV = 0;
+var humanA;
+var humanDamping;
+var humanLoss;
+var humanSize;
+var humanColor;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	zombieColor = color(255, 176, 75);
-	backgroundColor = color(242, 255, 0);
+	zombieColor = color(random(255), random(255), random(255));
+	humanColor = color(random(255), random(255), random(255));
+	//testZombieColor = color(255, 176, 75);
+
+	zombieDamping = random(-0.8, -0.95);
+	humanDamping = random(-0.8, -0.95);
+
+	zombieSize = random(70, 110);
+	humanSize = random(70, 110);
+
+	zombieA = random(0.05, 0.2);
+	humanA = random(0.05, 0.2);
+
+	zombieLoss = random(0.7, 0.95);
+	humanLoss = random(0.7, 0.95);
 }
 
 function draw() {
+	background(blood1, blood2, blood2);
 	noStroke();
-	background(255, 255, 255);
 	fill(zombieColor);
-	ellipse(zombieX, 50, size1, size2);
-	fill(150, 150, 200);
-	stroke(200, 0, 100);
-	ellipse(zombie2X, 200, 80, 80);
-	fill(100, 0, 200);
 	ellipse(windowWidth / 2, zombieY, zombieSize, zombieSize);
-	fill(0, 100, 200);
+	fill(humanColor);
 	ellipse(windowWidth / 2, humanY, humanSize, humanSize);
+	/*fill(testZombieColor);
+	ellipse(zombieX, 50, size1, size2);
+	fill(100, 200, 300);
+	stroke(200, 0, 100);
+	ellipse(zombie2X, 200, 80, 80);*/
 
 	zombieY += zombieV;
 	zombieV += zombieA;
-
+	humanY -= humanV;
+	humanV += humanA;
+	
 	if(zombieY + (zombieSize / 2) >= windowHeight) {
-		zombieY = windowHeight;
 		zombieV *= zombieDamping;
-		zombieSize *= 0.7;
+		zombieSize *= zombieLoss;
+		zombieColor = color(random(255), random(255), random(255));
 	}
 
-	if (zombieX >= 800) {
-		//zombieX = 0;
-		//redColor = random(255);
+	if(humanY - (humanSize / 2) <= 0) {
+		humanV *= humanDamping;
+		humanSize *= humanLoss;
+		humanColor = color(random(255), random(255), random(255));
+	}
+
+	if(zombieY + (zombieSize / 2) > humanY - (humanSize / 2) && humanSize > 1 && zombieSize > 1) {
+		zombieV *= zombieDamping;
+		zombieSize *= zombieLoss;
 		zombieColor = color(random(255), random(255), random(255));
+		
+		humanV *= humanDamping;
+		humanSize *= humanLoss;
+		humanColor = color(random(255), random(255), random(255));
+
+		blood1 -= 6;
+		blood2 -= 22;
+	}
+
+	if(zombieSize <= 1) {
+		zombieColor = color(blood1, blood2, blood2);
+	}
+
+	if(humanSize <= 1) {
+		humanColor = color(blood1, blood2, blood2);
+	}
+
+	/*if (zombieX >= 800) {
+		testZombieColor = color(random(255), random(255), random(255));
 		speed = speed + 1;
 		size1 = size1 - 1;
 		size2 = size2 - 1;
@@ -58,15 +105,14 @@ function draw() {
 	
 	if (zombieX <= 0) {
 		speed = speed * -1;
-		zombieColor = color(random(255), random(255), random(255));
+		testZombieColor = color(random(255), random(255), random(255));
 	}
 
-	//This keeps the circle from coming back after going away.
 	if (size1 == 0) {
 		zombieX = 1;
 		speed = 0;
 	}
 
 	zombieX = zombieX + speed;
-	zombie2X = zombie2X + 1;
+	zombie2X = zombie2X + 1;*/
 }
