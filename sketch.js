875ea17a43c4a2fe5eb469c83b4a6bed04793f4a
Amber <amberj.kolar@gmail.com>
2017-11-06 07:19:21 -0800

    Started working on a function to handle collisions

--- sketch.js
+++ sketch.js
@@ -1,13 +1,14 @@
 // Zombulator by Amber Kolar
-// CS 160 Exercise 19: Polymorphism
+// CS 160 Exercise 20: Collisions
 
 var backgroundColor;
 
 const MIN_SIZE = 5;
 const MAX_SIZE = 50;
-const POPULATION_SIZE = 200;
+const POPULATION_SIZE = 500;
 
 var population = [];
+
 var zombieCount = 0;
 var humanCount = 0;
 
@@ -15,7 +16,6 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   backgroundColor = color(245, 255, 245);
   initializePopulation();
-  countPopulation();
 }
 
 function draw() {
@@ -23,12 +23,44 @@ function draw() {
   noStroke();
   drawPopulation();
   movePopulation();
+  drawPopulationCounts();
+  handleCollisions();
 }
 
-// Zombies. Raaahh!
+function initializePopulation() {
+  for (var i = 0; i < POPULATION_SIZE; ++i) {
+    var humanoid_type = random(0, 100);
+    if (humanoid_type <= 50) {
+      population[i] = initializeZombie();
+      ++zombieCount;
+    } else {
+      population[i] = initializeHuman();
+      ++humanCount;
+    }
+  }
+}
+
+function drawPopulationCounts() {
+  stroke(0);
+  textSize(72);
+  textAlign(CENTER);
+  text("Zombies: " + zombieCount, width / 2, 100);
+  text("Humans: " + humanCount, width / 2, height - 100);
+}
+
+function drawPopulation() {
+  for (var i = 0; i < POPULATION_SIZE; ++i) {
+    population[i].draw();
+  }
+}
+
+function movePopulation() {
+  for (var i = 0; i < POPULATION_SIZE; ++i) {
+    population[i].move();
+  }
+}
 
 function initializeZombie() {
-  ++zombieCount;
   return {
     x: random(0, windowWidth),
     y: random(0, 200),
@@ -50,66 +82,51 @@ function initializeZombie() {
     draw: function() {
       fill(this.color);
       ellipse(this.x, this.y, this.size, this.size);
+    },
+    isTouching: function(target){
+
     }
   };
 }
 
-// Humans. Mmmm brains!
-
-function initializeHuman(index) {
-  ++humanCount;
+function initializeHuman() {
   return {
     x: random(0, windowWidth),
     y: random(windowHeight - 200, windowHeight),
     speed: random(0.25, 3),
     size: random(MIN_SIZE, MAX_SIZE),
     color: color(random(50, 150), random(50, 150), random(150, 255), 150),
-    draw: function() {
-      fill(this.color);
-      ellipse(this.x, this.y, this.size, this.size);
-    },
     move: function() {
-      var direction = random(0, 100);
-      if (direction < 20) {
-        this.x += this.speed;
-      } else if (direction < 40) {
-        this.x -= this.speed;
-      } else if (direction < 60) {
-        this.y += this.speed;
-      } else {
-        this.y -= this.speed;
+        var direction = random(0, 100);
+        if (direction < 20) {
+          this.x += this.speed;
+        } else if (direction < 40) {
+          this.x -= this.speed;
+        } else if (direction < 60) {
+          this.y += this.speed;
+        } else {
+          this.y -= this.speed;
+        }
+      },
+    draw: function() {
+        fill(this.color);
+        ellipse(this.x, this.y, this.size, this.size);
+    },
+    isTouching: function(target){
+      
+    }
+  };
+}
+
+function handleCollisions() {
+  for (var i = 0; i < POPULATION_SIZE; ++i) {
+    var attacker = population[i];
+    for (var j = i + 1; j < POPULATION_SIZE; ++j) {
+      var target = population[j];
+
+      if (attacker.isTouching(target)) {
+        print("Fight!")
       }
     }
-  }
-}
-
-//Population
-
-function initializePopulation() {
-  for (var i = 0; i < POPULATION_SIZE; ++i) {
-    var humanoid_type = random(1, 100);
-    if (humanoid_type <= 50) {
-      population[i] = initializeZombie();
-    } else {
-      population[i] = initializeHuman();
-    }
-  }
-}
-
-function countPopulation() {
-  textSize(32);
-  text(zombieCount, windowWidth/2, 50);
-  text(humanCount, windowWidth/2, 80);
-}
-
-function drawPopulation() {
-  for (var i = 0; i < POPULATION_SIZE; ++i) {
-    population[i].draw();
-  }
-}
-
-function movePopulation() {
-  for (var i = 0; i < POPULATION_SIZE; ++i) {
-    population[i].move();
   }
 }
