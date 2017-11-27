a99f44fd628fabb8c01dc6482efe722b3fb3ca12
Amber <amberj.kolar@gmail.com>
2017-11-27 04:13:48 -0800

    Gave random humans guns to randomly shoot at zombies with

--- sketch.js
+++ sketch.js
@@ -35,6 +35,7 @@ function handleCollisions() {
         if (target.humanoidType == "zombie") {
           --zombieCount;
           target.size = 0;
+          target.armed = 0;
         } else {
           --humanCount;
           ++zombieCount;
@@ -72,6 +73,7 @@ function initializePopulation() {
 }
 
 function drawPopulationCounts() {
+  fill(109, 255, 139, 150)
   stroke(0);
   textSize(72);
   textAlign(CENTER);
@@ -126,21 +128,42 @@ function initializeZombie() {
 function initializeHuman() {
   return {
     humanoidType: "human",
+    armed: random(0, 618),
     x: random(0, windowWidth),
     y: random(windowHeight - 200, windowHeight),
     speed: random(0.25, 3),
     size: random(MIN_SIZE, MAX_SIZE),
     color: color(random(50, 150), random(50, 150), random(150, 255), 150),
     move: function() {
-        var direction = random(0, 100);
-        if (direction < 20) {
+        var direction = random(0, 1000);
+        if (direction < 200) {
           this.x += this.speed;
-        } else if (direction < 40) {
+        } else if (direction < 400) {
           this.x -= this.speed;
-        } else if (direction < 60) {
+        } else if (direction < 600) {
           this.y += this.speed;
         } else {
           this.y -= this.speed;
+          if (this.armed > direction) {
+            fill(0, 0, 0);
+            bulletX = this.x;
+            bulletY = this.y - this.size/2;
+            bulletSize = 1.5;
+            while(bulletY >= 0) {
+              ellipse(bulletX, bulletY, bulletSize, bulletSize);
+              --bulletY;
+              for(var i = 0; i < POPULATION_SIZE; ++i) {
+                var target = population[i];
+                if ((bulletX - target.x <= target.size/2 || target.x - bulletX <= target.size/2) && bulletY <= target.y && target.size > 0 && target.humanoidType == "zombie" && bulletSize == 1.5) {
+                  --zombieCount;
+                  target.size = 0;
+                  bulletSize = 0;
+                  fill(125, 0, 0);
+                  ellipse(bulletX, bulletY, 10, 10);
+                }
+              }
+            }
+          }
         }
       },
     draw: function() {
